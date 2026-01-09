cf226d6901dd1e292e774b60c217a389c29ddfd6
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-01-10 00:31:51 +0530

    seperarted the function for the normal and alive block

--- src/sketch.js
+++ src/sketch.js
@@ -5,20 +5,42 @@ const GRAVITY = 0.1;
 const MAX_VELOCITY = 5;
 const FRICTION_COFFICIENT = 0.05;
 const ALIVE_BLOCK_DURATION = 100;
+const SCREEN_SPEED = 1;
 
 let ball;
+
+const createGrounds = () => {
+  const ground = [];
+  ground.push(new Ground(200, 500, 100));
+  return ground;
+};
+
+const createAliveGround = () => {
+  const ground = [];
+  ground.push(new AliveGround(100, 300, 100));
+  return ground;
+};
+
+const createSpikes = () => {
+  const ground = [];
+  ground.push(new SpikesGround(300, 500, 100));
+  return ground;
+};
+
 let ground;
-let ground2;
+let aliveGround;
+let spikes;
+
 function setup() {
   createCanvas(400, 800);
   ball = new Ball(100, 30, 5);
-  ground = new Ground(200, 500, 100);
-  ground2 = new AliveGround(100, 300, 100);
-  ground3 = new SpikesGround(300, 500, 100);
+  ground = createGrounds();
+  aliveGround = createAliveGround();
+  spikes = createSpikes();
 
-  ball.blocks.push(ground);
-  ball.blocks.push(ground2);
-  ball.spikesBlocks.push(ground3);
+  ground.map((ground) => ball.blocks.push(ground));
+  aliveGround.map((ground) => ball.blocks.push(ground));
+  spikes.map((ground) => ball.spikesBlocks.push(ground));
 }
 
 function draw() {
@@ -26,11 +48,13 @@ function draw() {
 
   ball.draw();
   ball.update();
-  ground.draw();
-  ground2.draw();
-  ground3.draw();
 
-  ground2.decreaseLife();
+  ground.map((each) => each.draw());
+  aliveGround.map((each) => each.draw());
+  aliveGround.map((each) => each.decreaseLife());
+
+  spikes.map((each) => each.draw());
+
   if (ball.isGameOver()) {
     background(1);
     noLoop();
